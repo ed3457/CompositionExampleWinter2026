201c414e794517093508d238b49b09503a17e1de
Kamel R <kamel.r@wayne.edu>
2026-02-02 19:06:14 -0500

    created a class for Department

---
+++ Department.cpp
@@ -0,0 +1,11 @@
+#include "Department.h"
+
+void Department::setAddress(Address a)
+{
+	deptAddress = a;
+}
+
+Address Department::getAddress()
+{
+	return deptAddress;
+}
---
+++ Department.h
@@ -0,0 +1,12 @@
+#pragma once
+#include "Address.h"
+class Department
+{
+private:
+	Address deptAddress;
+public:
+	void setAddress(Address a);
+	Address getAddress();
+
+};
+
